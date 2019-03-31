3a5e7b5dd5342e9fb9d77a0078ece9d3a923e14b
Kyazzie7 <kyazzie7@asu.edu>
2019-03-31 00:05:37 -0700

    Add Account Class

---
+++ USAA_Project/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace USAA_Project
+{
+    public class Account
+    {
+
+        public int id;
+        public string userId;
+        public string password;
+        public string firstName;
+        public string lastName;
+        public string email;
+    }
+}
