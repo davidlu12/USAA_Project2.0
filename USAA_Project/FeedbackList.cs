b78ca86baf08292baffb945fa6459993426faf94
Kyazzie7 <kyazzie7@asu.edu>
2019-04-01 23:37:20 -0700

    Reset -- hard change

---
+++ USAA_Project/FeedbackList.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace USAA_Project
+{
+    public class FeedbackList
+    {
+        public string reviewer;
+        public string department;
+        public int rating;
+        public string comment;
+        public int approval;
+    }
+}
--- USAA_Project/JavaScript.js
+++ USAA_Project/JavaScript.js
@@ -1,1 +1,1 @@
-﻿push changes
+﻿
