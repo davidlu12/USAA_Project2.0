f78ae1d4415d1c9c1e74e8dabcd202ac9fbc45ec
Kyazzie7 <kyazzie7@asu.edu>
2019-04-06 00:48:59 -0700

    //Changed AccountPanels to DepartmentsPanel Same in JS

--- USAA_Project/JavaScript.js
+++ USAA_Project/JavaScript.js
@@ -1,7 +1,7 @@
 ﻿//we're using a stacked card approach for our main viewing area
 //this array holds the ids of our cards and the method
 //allows us to easly switch the interface from one to the other
-var contentPanels = ['logonPanel', 'newAccountPanel', 'accountsPanel', 'listPanel', 'editAccountPanel', 'requestsPanel'];
+var contentPanels = ['logonPanel','departmentsPanel', 'listPanel', 'editAccountPanel', 'requestsPanel'];
 //this function toggles which panel is showing, and also clears data from all panels
 function showPanel(panelId) {
     clearData();
@@ -26,59 +26,33 @@ function clearData() {
 
 //HERE'S AN EXAMPLE OF AN AJAX CALL WITH JQUERY!
 function LogOn(userId, pass) {
-    //the url of the webservice we will be talking to
     var webMethod = "AccountServices.asmx/LogOn";
-    //the parameters we will pass the service (in json format because curly braces)
-    //note we encode the values for transmission over the web.  All the \'s are just
-    //because we want to wrap our keynames and values in double quotes so we have to
-    //escape the double quotes (because the overall string we're creating is in double quotes!)
     var parameters = "{\"uid\":\"" + encodeURI(userId) + "\",\"pass\":\"" + encodeURI(pass) + "\"}";
 
     //jQuery ajax method
     $.ajax({
-        //post is more secure than get, and allows
-        //us to send big data if we want.  really just
-        //depends on the way the service you're talking to is set up, though
         type: "POST",
-        //the url is set to the string we created above
         url: webMethod,
-        //same with the data
         data: parameters,
-        //these next two key/value pairs say we intend to talk in JSON format
         contentType: "application/json; charset=utf-8",
         dataType: "json",
-        //jQuery sends the data and asynchronously waits for a response.  when it
-        //gets a response, it calls the function mapped to the success key here
         success: function (msg) {
-            //the server response is in the msg object passed in to the function here
-            //since our logon web method simply returns a true/false, that value is mapped
-            //to a generic property of the server response called d (I assume short for data
-            //but honestly I don't know...)
             if (msg.d) {
                 //server replied true, so show the accounts panel
-                showPanel('accountsPanel');
+                showPanel('departmentsPanel');
                 LoadAccounts();
             }
             else {
-                //server replied false, so let the user know
-                //the logon failed
                 alert("login failed");
             }
         },
         error: function (e) {
-            //if something goes wrong in the mechanics of delivering the
-            //message to the server or the server processing that message,
-            //then this function mapped to the error key is executed rather
-            //than the one mapped to the success key.  This is just a garbage
-            //alert becaue I'm lazy
             alert("boo...");
         }
     });
 }
 
-//when we retrieve accounts, we'll put them here
-//so that we can reference them later for admins
-//that may want to edit accounts
+//List all accounts for admins to view
 var accountsArray;
 //to begin with, we assume that the account is not an admin
 var admin = false;
